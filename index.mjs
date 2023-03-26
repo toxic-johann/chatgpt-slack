8439bd27de2ffb18897e9a04a7c156419b6db32c
Heng Zhong <heng@tubi.tv>
2023-03-26 21:50:40 +0800

    feat: use chatgpt to optimize SD prompt

--- index.mjs
+++ index.mjs
@@ -68,16 +68,20 @@ app.message('git pull', async ({ message, say }) => {
 
 app.message(/^draw|画/i, async ({ message, say }) => {
   console.log(message);
+  console.log('transport message to prompt through chatgpt');
+  const thread_ts = message.thread_ts || message.ts;
+  const command = message.text.replace(/^draw|画/i, "");
+  const prompt = await chatgpt.sendMessage(`transport the following message into a stable defussion prompt in english and only return the english prompt without explanation: ${command}`);
   const prediction = await replicate
     .model(
       "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
     )
     .predict({
-      prompt: message.text.replace(/^draw|画/i, ""),
+      prompt: prompt.text,
     })
   console.log(prediction);
   // say() sends a message to the channel where the event was triggered
-  await say(prediction.output[0]);
+  await say({ text: prediction.output[0], thread_ts });
 });
 
 (async () => {
