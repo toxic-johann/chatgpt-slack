89a5d90c393aa0331d97c78d2bca43befd8049ff
Heng Zhong <heng@tubi.tv>
2023-03-27 08:36:39 +0800

    fix: fix multiple line bug

--- index.mjs
+++ index.mjs
@@ -58,7 +58,7 @@ const chatgpt = new ChatGPTAPI({
 });
 
 /* Add functionality here */
-app.message(/^(?!(git pull|draw|画)).*$/, async ({ message, say }) => {
+app.message(/^(?!(git pull|draw|画))(.|\s)*$/, async ({ message, say }) => {
   console.log(message);
   console.log('calling chatgpt');
   const thread_ts = message.thread_ts || message.ts;
@@ -109,13 +109,28 @@ app.message('git pull', async ({ message, say }) => {
   await run(`pm2 restart all`);
 });
 
-app.message(/.*/, async ({ message, say }) => {console.log(message)});
-
 app.message(/^(draw|画)/i, async ({ message, say }) => {
   console.log(message);
   console.log('transport message to prompt through chatgpt');
   const thread_ts = message.thread_ts || message.ts;
-  const command = message.text.replace(/^draw|画/i, "");
+  const result = await chatgpt.sendMessage(`This is a draw request, “${message.text}”. Please extract the content that I want to draw and name it item A. For example, "draw a dog" should generate item A as "a dog". And then optimize the item A you just generate as a stable diffusion prompt in English. Return me the result in the following format: "Item A:xxx; Prompt: xxx".`);
+  const promptMatch = result.text.match(/Prompt:(.*?)$/);
+  await say({ text: result.text, thread_ts });
+  const prediction = await replicate
+    .model(
+      "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
+    )
+    .predict({
+      prompt: promptMatch && promptMatch.length > 0 ? promptMatch[1] : prompt.text,
+    })
+  console.log(prediction);
+  // say() sends a message to the channel where the event was triggered
+  await say({ text: prediction.output[0], thread_ts });
+});
+
+app.message(/^SD:/i, async ({ message, say }) => {
+  console.log(message);
+  const command = message.text.replace(/^SD/i, "");
   const result = await chatgpt.sendMessage(`This is a draw request, “${message.text}”. Please extract the content that I want to draw and name it item A. For example, "draw a dog" should generate item A as "a dog". And then optimize the item A you just generate as a stable diffusion prompt in English. Return me the result in the following format: "Item A:xxx; Prompt: xxx".`);
   const promptMatch = result.text.match(/Prompt:(.*?)$/);
   await say({ text: result.text, thread_ts });
