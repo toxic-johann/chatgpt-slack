786aaf904bdc990c0a7c04d034af660a0e11b9aa
钟恒 <toxic.johann@gmail.com>
2023-04-02 20:48:55 +0800

    feat: calculate duration

--- src/route/remind.mjs
+++ src/route/remind.mjs
@@ -1,11 +1,13 @@
 import { getThreadTs } from '../selectors/message.mjs';
 import chatgpt from '../utils/chatgpt.mjs';
 import timeConvert from '../utils/time-convert.mjs';
+import dayjs from 'dayjs';
 
 export const regexp = /^(r:|remind|提醒)/i;
 
 export default async ({ message, say }) => {
   const thread_ts = getThreadTs(message);
+  const clientTime = dayjs.unix(message.ts);
   const remindInfo = await chatgpt.sendMessage(`
   I want to extract information from the reminder message. This kind of message usually includes two pieces of information. One is the time message. The other one is the work that I need to do at that time.
   For example, Remind me to buy tickets at 5 pm should split into these two parts.
@@ -27,4 +29,13 @@ export default async ({ message, say }) => {
     duration,
   } = await timeConvert(timeString, (text) => say({ text, thread_ts }));
   await say({ text: `Time: ${time}\nDuration: ${duration}\nWork: ${workString}`, thread_ts });
+  if (/\d+-\d+:\d+:\d+/.test(duration)) {
+    const [day, hour, minute, second] = duration.split(/-|:/);
+    const estimateTime = clientTime
+      .add(day, 'day')
+      .add(hour, 'hour')
+      .add(minute, 'minute')
+      .add(second, 'second');
+    await say({ text: `Your current time is ${clientTime.format()}. Your reminder is at ${estimateTime.format()}`});
+  }
 };
