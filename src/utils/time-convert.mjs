3901b112fbe21ecb0b733a899c0adf40fc26d677
Heng Zhong <heng@tubi.tv>
2023-04-02 19:17:53 +0800

    feat: update time convert prompt

--- src/utils/time-convert.mjs
+++ src/utils/time-convert.mjs
@@ -1,11 +1,15 @@
 import chatgpt from './chatgpt.mjs';
 
 export default async (message, log) => {
-  const res = await chatgpt.sendMessage(`Can you help me to convert two types of time messages into specific formats? The first type of message is a duration and can have any format, as long as it includes a number and a time unit up to days (ex: “3 hours later”, “5 days from now”). We want to convert these durations into the format DD-hh:mm:ss, with days represented by two digits and hours, minutes, and seconds represented by two digits each. The second type of message is a specific time and can have any format (ex: “May 1st 8am”, “9/15/2023 3pm”, "10pm"). We want to convert these specific times into the format YYYY-MM-DD hh:mm:ss. If the user doesn’t provide the larger unit, keep the alphabet. If the user doesn’t provide the smaller unit, replace it with 0. Please note that we only accept units up to days and we ignore months and years. If the input message is invalid, return an error message starting with “Error:“.
+  const res = await chatgpt.sendMessage(`Can you help me to convert two types of time messages into specific formats?
+  The first type of message is a duration and can have any format, as long as it includes a number and a time unit up to days (ex: “3 hours later”, “5 days from now”). We want to convert these durations into the format DD-hh:mm:ss, with days represented by two digits and hours, minutes, and seconds represented by two digits each.
+  The second type of message is part of a specific time and can have any format (ex: “May 1st 8am”, “9/15/2023 3pm”, “10pm”). You should convert it into the format yyyy-mm-dd hh:mm:ss. If any part of the string is missing, please replace it with * in the output.
+  If the input message is invalid, return an error message starting with “Error:“.
+  You should return the result only without any explanations.
   Final output:
-  duration: [DD-hh:mm:ss]
-  Time: [YYYY-MM-DD hh:mm:ss]
-  The offical input is ${message}`);
+    duration: [DD-hh:mm:ss]
+    Time: [YYYY-MM-DD hh:mm:ss]
+  My message is "${message}"`);
   if (typeof log === 'function') {
     log(res.text);
   }
