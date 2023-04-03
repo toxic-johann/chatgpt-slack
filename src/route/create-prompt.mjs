92d2798be8278d985c7e0cd7171cea777eff8749
Heng Zhong <heng@tubi.tv>
2023-04-03 08:16:34 +0800

    feat: support multiple language

--- src/route/create-prompt.mjs
+++ src/route/create-prompt.mjs
@@ -1,12 +1,15 @@
+import ISO6391 from 'iso-639-1';
+
 import { getThreadTs } from '../selectors/message.mjs';
 import chatgpt from '../utils/chatgpt.mjs';
 import conversationCache from '../utils/conversation-cache.mjs';
 
-export const regExp = /^cp:/i;
+export const regExp = new RegExp(`^cp(-(${ISO6391.getAllCodes().join('|')}))?:`, 'i');
 
 export default async ({ message, say }) => {
   const thread_ts = getThreadTs(message);
   const parentMessageId = conversationCache.get(thread_ts);
+  const languageRequirement = message.text.match(regExp)[2];
   const res = await chatgpt.sendMessage(`
 I want you to become my Expert Prompt Creator. Your goal is to help me craft the best possible prompt for my needs. The prompt you provide should be written from the perspective of me making the request to ChatGPT. Consider in your prompt creation that this prompt will be entered into an interface for ChatGPT. The process is as follows:
 You will generate the following sections:
@@ -18,6 +21,7 @@ Questions:
 {ask any questions pertaining to what additional information is needed from me to improve the prompt (max of 1). If the prompt needs more clarification or details in certain areas, ask questions to get more information to include in the prompt}
 I will provide my answers to your response which you will then incorporate into your next response using the same format. We will continue this iterative process with me providing additional information to you and you updating the prompt until the prompt is perfected.
 Remember, the prompt we are creating should be written from the perspective of me making a request to ChatGPT. Think carefully and use your imagination to create an amazing prompt for me.
+${languageRequirement ? `Please respond me in ${ISO6391.getName(languageRequirement)}` : ''}
 My idea is the below one.
 ${message.text.replace(regExp, '')}`, {
     parentMessageId,
