6c6870617b68ab5a2612b059fd40a9f04fbdefc6
Heng Zhong <heng@tubi.tv>
2023-04-09 22:48:21 +0800

    feat: know how to use elasticsearch

--- src/merck/index.mjs
+++ src/merck/index.mjs
@@ -1,11 +1,16 @@
+/* eslint-disable no-underscore-dangle */
 import { Client } from '@elastic/elasticsearch';
 import fs from 'fs';
+import path from 'path';
+import TurndownService from 'turndown';
+import url from 'url';
 import { ELASTIC_SEARCH_PASSWORD } from './config.mjs';
 
-// const client = new Client({
-//   cloud: { id: '<cloud-id>' },
-//   auth: { apiKey: 'base64EncodedKey' },
-// });
+const turndownService = new TurndownService();
+turndownService.remove(['script', 'img']);
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const ca = fs.readFileSync('./temp/http_ca.crt');
 
@@ -22,44 +27,70 @@ const client = new Client({
 });
 
 async function run() {
+  const dir = path.resolve(__dirname, '../../../MSDZHConsumerMedicalTopics');
+  const files = fs.readdirSync(dir).filter((p) => p.endsWith('}.html'));
+  const response = await client.deleteByQuery({
+    index: 'merk-zh',
+    body: {
+      query: {
+        match_all: {},
+      },
+    },
+  });
+  console.log(response);
+  // const promises = files.map(async (fileName, index) => {
+  //   const filePath = path.resolve(dir, fileName);
+  //   const html = fs.readFileSync(filePath, 'utf8');
+  //   console.warn(filePath, index);
+  //   const match = html.match(/<main\b[^>]*>([\s\S]*?)<\/main>/i);
+  //   if (!match) return;
+  //   const title = html.match(/<title\b[^>]*>(.*?)<\/title>/i)[1];
+  //   const content = match[0];
+  //   const markdown = turndownService.turndown(content);
+  //   await client.index({
+  //     index: 'merk-zh',
+  //     document: {
+  //       title,
+  //       content: markdown,
+  //     },
+  //   });
+  // });
+
+  // await Promise.all(promises);
+
+  // console.warn(title, markdown);
+
   // Let's start by indexing some data
-  await client.index({
-    index: 'game-of-thrones',
-    document: {
-      character: 'Ned Stark',
-      quote: 'Winter is coming.',
-    },
-  });
 
-  await client.index({
-    index: 'game-of-thrones',
-    document: {
-      character: 'Daenerys Targaryen',
-      quote: 'I am the blood of the dragon.',
-    },
-  });
+  // await client.index({
+  //   index: 'game-of-thrones',
+  //   document: {
+  //     character: 'Daenerys Targaryen',
+  //     quote: 'I am the blood of the dragon.',
+  //   },
+  // });
 
-  await client.index({
-    index: 'game-of-thrones',
-    document: {
-      character: 'Tyrion Lannister',
-      quote: 'A mind needs books like a sword needs a whetstone.',
-    },
-  });
+  // await client.index({
+  //   index: 'game-of-thrones',
+  //   document: {
+  //     character: 'Tyrion Lannister',
+  //     quote: 'A mind needs books like a sword needs a whetstone.',
+  //   },
+  // });
 
   // here we are forcing an index refresh, otherwise we will not
   // get any result in the consequent search
-  await client.indices.refresh({ index: 'game-of-thrones' });
+  await client.indices.refresh({ index: 'merk-zh' });
 
   // Let's search!
   const result = await client.search({
-    index: 'game-of-thrones',
+    index: 'merk-zh',
     query: {
-      match: { quote: 'winter' },
+      match: { content: '急性肠系膜缺血' },
     },
   });
 
-  console.log(result.hits.hits);
+  console.log(result.hits.hits.map((hit) => hit._source.title));
 }
 
 run().catch(console.log);
