7fb928c5d6dc9f0d196e20b480483ea49af3a251
Heng Zhong <heng@tubi.tv>
2023-04-20 22:50:20 +0800

    fix: node-replicate update error

--- src/route/draw.mjs
+++ src/route/draw.mjs
@@ -13,13 +13,9 @@ export const route = async ({ message, say }) => {
   const result = await chatgpt.sendMessage(`This is a draw request, “${message.text}”. Please extract the content that I want to draw and name it item A. For example, "draw a dog" should generate item A as "a dog". And then optimize the item A you just generate as a stable diffusion prompt in English. Return me the result in the following format: "Item A:xxx; Prompt: xxx".`);
   const promptMatch = result.text.match(/Prompt:(.*?)$/);
   await say({ text: result.text, thread_ts });
-  const prediction = await replicate
-    .model(
-      'stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf',
-    )
-    .predict({
-      prompt: promptMatch && promptMatch.length > 0 ? promptMatch[1] : prompt.text,
-    });
+  const model = 'stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf';
+  const input = { prompt: promptMatch && promptMatch.length > 0 ? promptMatch[1] : prompt.text };
+  const prediction = await replicate.run(model, input);
   console.log(prediction);
   // say() sends a message to the channel where the event was triggered
   await say({ text: prediction.output[0], thread_ts });
