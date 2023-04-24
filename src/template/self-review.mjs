514c8b668047c235f9ac7f90d03738485620517e
Heng Zhong <heng@tubi.tv>
2023-04-24 23:13:49 +0800

    feat: use system messsage for language requirement

--- src/template/self-review.mjs
+++ src/template/self-review.mjs
@@ -15,12 +15,12 @@ export default ({
     : message.text.match(regExp)[2];
   const description = `
   ${template || getTemplate(text)}
-  ${languageRequirement ? `Please respond me in ${ISO6391.getName(languageRequirement)}` : ''}
   My ${inputName} is the below one.
   ${message.text.replace(regExp, '')}`;
   console.log(description);
   const res = await chatgpt.sendMessage(description, {
     parentMessageId,
+    systemMessage: languageRequirement ? `Please respond me in ${ISO6391.getName(languageRequirement)}` : undefined,
   });
   conversationCache.set(thread_ts, res.id);
   // say() sends a message to the channel where the event was triggered
